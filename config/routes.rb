d69a645d2403311d089e03e0f2c9dc4f181596e2
suzuki02yo@gmail.com <suzuki02yo@gmail.com>
2016-04-22 16:27:50 +0000

    top contlloer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'top/index'
+
+  root to: "top#index" 
 
   resources :blogs
   # The priority is based upon order of creation: first created -> highest priority.
