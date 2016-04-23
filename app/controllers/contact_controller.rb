d37121123e9ae4fea5d6023c0df36f13b75a6301
suzuki02yo@gmail.com <suzuki02yo@gmail.com>
2016-04-23 15:49:27 +0000

    toiawase_jissou

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,13 +1,15 @@
 class ContactController < ApplicationController
   def new
-    @contact = Contact.new(contact_params)
+    
+    @contact = Contact.new
+  end
+
+  def confirm
+        @contact = Contact.new(contact_params)
     if @contact.valid?
     else
       render action: 'new'
     end
-  end
-
-  def confirm
   end
 
   def thanks
@@ -15,9 +17,9 @@ class ContactController < ApplicationController
   end
   
   private
-   def contact_params
-    params.require(:contact).permit(:name, :email, :messsage)
-   end
-  
-  
+    def contact_params
+      params.require(:contact).permit(:name, :email, :message)
+    end
+
+   
 end
