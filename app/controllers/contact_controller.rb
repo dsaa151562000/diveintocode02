8728349d2901deedee4fe5ca28ccb3636c2ba3d3
suzuki02yo@gmail.com <suzuki02yo@gmail.com>
2016-05-14 02:13:46 +0000

    お問い合わせメール設定3

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -9,8 +9,7 @@ class ContactController < ApplicationController
        @contact = Contact.new(contact_params)
         
     if @contact.valid?
-
-      
+       @contact.gsub(/\r\n|\r|\n/, "<br />")
     else
       render action: 'new'
     end
