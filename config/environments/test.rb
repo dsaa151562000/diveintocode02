5f329eff3004893761ec6b45607d4a7af9e24290
suzuki02yo@gmail.com <suzuki02yo@gmail.com>
2016-05-15 05:19:34 +0000

    　ファイル復活

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,13 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      user_name: ENV["GMAIL_ADDRESS"],
+      password: ENV["GMAIL_PASSWORD"],
+      authentication:       'plain',
+      enable_starttls_auto: true  }
 end
