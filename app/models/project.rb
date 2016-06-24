c02c8734461a8a651d115c89617c62d364f2d0e6
suzuki02yo@gmail.com <suzuki02yo@gmail.com>
2016-06-24 15:30:38 +0000

    day16_comp

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,5 +6,6 @@ class Project < ActiveRecord::Base
   has_many :memberships, dependent: :destroy
   validates :title, presence: { message: "プロジェクト名を入力してください" }
   validates  :content, presence: { message: "内容を入力してください" }
+  validates :deadline, timeliness: { on_or_after: :now }
 
 end
