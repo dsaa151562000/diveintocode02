52997bdeb92cbce37756c0132921f0f140a109ca
suzuki02yo@gmail.com <suzuki02yo@gmail.com>
2016-07-23 07:20:28 +0000

    deviceの調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,7 +55,7 @@ class User < ActiveRecord::Base
                         password: Devise.friendly_token[0,20]
                         )
      user.skip_confirmation!
-     user.save
+     user.save!
    end 
   user
   end
