b18e8afe32ba245fae48dc61fb9e38b1c0ebdfd9
suzuki02yo@gmail.com <suzuki02yo@gmail.com>
2016-07-24 01:34:13 +0000

    mailser production.rb gmailへ変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,14 +90,24 @@ Rails.application.configure do
   #     authentication:       'plain',
   #   }
   
-   config.action_mailer.delivery_method = :smtp
-   config.action_mailer.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'heroku.com',
-      :enable_starttls_auto => true
-    }
+#   config.action_mailer.delivery_method = :smtp
+#   config.action_mailer.smtp_settings = {
+#       :address        => 'smtp.sendgrid.net',
+#       :port           => '587',
+#       :authentication => :plain,
+#       :user_name      => ENV['SENDGRID_USERNAME'],
+#       :password       => ENV['SENDGRID_PASSWORD'],
+#       :domain         => 'heroku.com',
+#       :enable_starttls_auto => true
+#     }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      user_name: ENV["GMAIL_ADDRESS"],
+      password: ENV["GMAIL_PASSWORD"],
+      authentication:       'plain',
+     }
 end
